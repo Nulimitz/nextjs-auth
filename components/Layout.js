584115f335c376c7de0fe6d89ec6557e377ed87c
Mark Winters <nulimitz360@gmail.com>
2022-08-27 10:08:19 -0600

    Fixed the navbar link

--- components/Layout.js
+++ components/Layout.js
@@ -21,7 +21,7 @@ export default function Layout({ title, children }) {
       <nav class="navbar navbar-expand-lg bg-light fixed-top">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">
-            Navbar
+            NextJS
           </a>
           <button
             class="navbar-toggler"
@@ -35,14 +35,14 @@ export default function Layout({ title, children }) {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="/">
                   Link
                 </a>
               </li>
